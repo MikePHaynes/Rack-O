5ea0033cf80245324aaa7dae86d4e7f69c2e1a63
MikePHaynes <86630042+MikePHaynes@users.noreply.github.com>
2022-07-16 13:49:32 -0700

    Added Node class.

---
+++ Rack-O/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rack_O
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+        public void SetNext(Node next) 
+        { 
+            Next = next;
+        }
+    }
+}
